33dc80bd0f4f5095e51ebe0b9a8f10d5f50aebd0
root <zain@accucalc.com>
2021-01-03 14:46:20 -0600

    check in page operational

--- js/checkin.js
+++ js/checkin.js
@@ -1,14 +1,12 @@
 // js for checkin
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-apiKey: "AIzaSyBQiIjrNDtP2A5-gNAOakkaeieoLWvpwqQ",
-authDomain: "hourtracker-2b6f8.firebaseapp.com",
-projectId: "hourtracker-2b6f8",
-storageBucket: "hourtracker-2b6f8.appspot.com",
-messagingSenderId: "82969866110",
-appId: "1:82969866110:web:5a089299065444cbea0d2f",
-measurementId: "G-DS4GRL509N"
+	apiKey: "AIzaSyBQiIjrNDtP2A5-gNAOakkaeieoLWvpwqQ",
+	authDomain: "hourtracker-2b6f8.firebaseapp.com",
+	projectId: "hourtracker-2b6f8",
+	storageBucket: "hourtracker-2b6f8.appspot.com",
+	messagingSenderId: "82969866110",
+	appId: "1:82969866110:web:5a089299065444cbea0d2f",
+	measurementId: "G-DS4GRL509N"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -22,75 +20,130 @@ you are about to use javascript you may end up throwing your device out the wind
 
 var db = firebase.firestore();
 
-var time;
+var timeIn;
+var timeOut;
+
+var display = document.querySelector('#recorded-time-display');
+var hourSelect = document.querySelector('#select-hour');
 
 function submitData(event){
 	
 	event.preventDefault();
+	// to prevent reload of the webpage on submit
 	
 	console.log('submit');
-	console.log(time);
+	console.log(timeIn);
+
+	var year = String(timeIn.getFullYear());
+	var month = String(timeIn.getMonth() +1);
+	// month +1 because index starts at 0
+	var day = String(timeIn.getDate());
+
+	month = (month.length === 1)? '0' + month : month;
+	day = (day.length === 1)? '0' + day : day;
+
+	var CLOCK_IN_HOUR = String(timeIn.getHours());
+	var CLOCK_IN_MINUTE = String(timeIn.getMinutes());
 	
-	var timeStayedToday = document.querySelector('#time-select').value;
+	var CLOCK_OUT_HOUR = document.querySelector('#select-hour').value;
+	var CLOCK_OUT_MINUTE = document.querySelector('#select-minute').value;
+
+	var timeOut = new Date(year,month-1,day,CLOCK_OUT_HOUR,CLOCK_OUT_MINUTE);
+	timeIn = new Date(year,month-1,day,CLOCK_IN_HOUR,CLOCK_IN_MINUTE);
+	// redefined to remove seconds and ms
+
+	var elapsedTime = timeOut.getTime() - timeIn.getTime();
+
+	console.log(elapsedTime);
+
 	var studentID = document.querySelector('#student-id-box').value;
 	var type = document.querySelector('#hour-type').value;
 	
-	var year = String(time.getFullYear());
-	var month = String(time.getMonth());
-	var date = String(time.getDate());
-	var hour = String(time.getHours());
-	var minute = String(time.getMinutes());
-	var second = String(time.getSeconds());
-	
-	
-	var serviceHoursTemp;
-	var shopHoursTemp;
-	
-	var docName = month + date + year;
-	var timeHMS = hour+minute+second;
+	var docName = month + day + year;
+	var docRefStudent = db.collection("Users").doc(studentID);
+	var docRefLog = docRefStudent.collection("logs").doc(docName);
+
+	var newShopHours;
+	var newServiceHours;
+
+	if(elapsedTime <= 0){
+		alert('negative hours');
+	}
+
+	if(type === "shop"){
+		newShopHours = elapsedTime/3600000; // converting ms to hours
+		newServiceHours = 0;
+		console.log(newShopHours);
+	} else if(type === "service"){
+		newServiceHours = elapsedTime/3600000;
+		newShopHours = 0;
+		console.log(newServiceHours);
+	}
 	
 	if(studentID.length == 8){
-				
-		db.collection("Users").doc(studentID).collection("logs").doc(docName).set({
-			
-			timeIn: timeHMS,
-			timeStayed: timeStayedToday,
-			
-			hourType: type //service or shop				
-		});
 		
-		db.collection("Users").doc(studentID).get().then(function(doc){
-			serviceHoursTemp = doc.data().serviceHours;
-			shopHoursTemp =doc.data().shopHours;
-		});
-		
-		if(type == "service"){
-			db.collection("Users").doc(studentID).set({
-				serviceHours : serviceHoursTemp + parseInt(timeStayedToday)
-			});
-		} else {
-			db.collection("Users").doc(studentID).set({
-				shopHours : shopHoursTemp + parseInt(timeStayedToday)
-			});
-		}
+		docRefStudent.get().then(function(doc){
 
+			if(doc.exists){
+
+				docRefLog.set({
 		
+					clockInHour: CLOCK_IN_HOUR,
+					clockInMinute: CLOCK_IN_MINUTE,
+					
+					clockOutHour: CLOCK_OUT_HOUR,
+					clockOutMinute: CLOCK_OUT_MINUTE,
+					
+					hourType: type
+				});
+
+				docRefStudent.set({
+					firstName: doc.data().firstName,
+					lastName: doc.data().lastName,
+					teamNumber: doc.data().teamNumber,
+					subteam: doc.data().subteam,
+					role: doc.data().role,
+					shopHours: doc.data().shopHours + newShopHours,
+					serviceHours: doc.data().serviceHours + newServiceHours
+				});
+				display.innerHTML = 'Your response was recorded';
+
+			}else{
+				
+				alert('Human #'+studentID+' Not found');
+			}
+				
+		});
+				
 		console.log('data in Firebase');
 	
 	} else {
 		alert("check your StudentID, it should be 8 chars long");
 	}
 	
-	
 }
 
 function setup(){
 	
 	//console.log('setup');
+	timeIn = new Date();
 	
-	time = new Date();
-	var display = document.querySelector('#recorded-time-display');
-	display.innerHTML = time;
+	// Month +1 because month index start at 0 
+	display.innerHTML = 
+		String(timeIn.getMonth()+1) + '-'+
+		String(timeIn.getDate()) + '-'+
+		String(timeIn.getFullYear()) + ' '+
+		String(timeIn.getHours()) + ':' +
+		String(timeIn.getMinutes())
+	;
+
+	for(var currentHour = timeIn.getHours(); currentHour<24; currentHour++ ){
+		var opt = document.createElement("option");
+		opt.value = currentHour;
+		opt.innerHTML = currentHour;
+		hourSelect.appendChild(opt);
+	}
+	
 	
 	var form = document.querySelector('#form');
 	
