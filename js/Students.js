5b34ede3b8e63eda924a881df676ed23ad6e5e40
root <zain@accucalc.com>
2021-01-11 09:49:45 -0600

    removed extraneous files

--- js/Students.js
+++ js/Students.js
@@ -220,6 +220,7 @@ function resetTable(){
 	}
 	
 }
+
 /*
 function removeAllChildren(thing){
 	while(thing.firstChild){
@@ -227,6 +228,363 @@ function removeAllChildren(thing){
 	}
 }
 */
+
+function search(){
+	
+	console.log('search');
+	resetTable();
+
+	var selectedID = IDBox.value;
+	var selectedFirstName = firstNameBox.value;
+	var selectedLastName = lastNameBox.value;
+	var selectedTeam = teamSelect.value;
+	var selectedRole = roleSelect.value;
+	var selectedSubteam = subteamSelect.value;
+	
+	var people = db.collection("Users");
+	var filteredPeople;
+	
+	if(selectedID.length == 8){
+		console.log('ID search');
+		filteredPeople = people.doc(selectedID).get().then(
+			function(doc){
+				if(doc.exists){
+					renderRowHTML(doc);
+				} else {
+					dataTableHTML = '<tr><td>not found</td></tr>';
+					dataTableBody.innerHTML = dataTableHTML;
+				}
+			}
+		);
+
+	} else if(selectedID.length == 0){
+		filteredPeople = people;
+		if(selectedFirstName.length >0){
+			filteredPeople = filteredPeople.where("firstName","==", selectedFirstName);
+		}
+		if(selectedLastName.length > 0){
+			filteredPeople = filteredPeople.where("lastName", "==", selectedLastName);
+		}
+		if(selectedTeam != "none"){
+			filteredPeople = filteredPeople.where("teamNumber","==", selectedTeam);		
+		}
+		if(selectedRole != "none"){
+			filteredPeople = filteredPeople.where("role","==",selectedRole);
+		}
+		if(selectedSubteam != "none"){
+			filteredPeople = filteredPeople.where("subteam","==",selectedSubteam);	
+		}
+		
+		filteredPeople.get()
+		.then(function(querySnapshot) {
+			//dataTableHTML = '';
+			querySnapshot.forEach(function(doc) {
+				console.log(doc.id, " => ", doc.data());
+				renderRowHTML(doc);
+			});
+			//dataTableBody.innerHTML = dataTableHTML;
+		})
+		.catch(function(error) {
+			console.log("Error getting documents: ", error);
+		});
+	} else {
+		alert('invalid ID');
+	}
+
+}
+
+/*
+function expandRow(row){
+
+	console.log('expandRow'+row.id);
+
+	try{
+		rowTemp.removeChild(hourTable);
+	}catch(err){
+		console.log(err);
+	}
+
+	rowTemp = row;
+
+	hourTable.setAttribute('class','hourTable');
+
+	var studentLogRef = db.collection("Users").doc(row.id).collection('logs');
+	
+	var titleRow = document.createElement('tr');
+
+	var titleDate = document.createElement('th');
+	var titleClockIn = document.createElement('th');
+	var titleClockOut = document.createElement('th');
+	var titleType = document.createElement('th');
+
+	titleDate.innerHTML = 'Date';
+	titleClockIn.innerHTML = 'Clock In';
+	titleClockOut.innerHTML = 'Clock Out';
+	titleType.innerHTML = 'Type';
+
+	titleRow.appendChild(titleDate);
+	titleRow.appendChild(titleClockIn);
+	titleRow.appendChild(titleClockOut);
+	titleRow.appendChild(titleType);
+
+	hourTable.appendChild(titleRow);
+
+	studentLogRef.get()
+	.then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+			
+			console.log(doc.id, " => ", doc.data());
+			
+			var log = document.createElement('tr');
+			
+			var clockInData = document.createElement('td');
+			var clockOutData = document.createElement('td');
+			var dateData = document.createElement('td');
+			var typeData = document.createElement('td');
+
+			clockInData.innerHTML = doc.data().clockInHour + ":" + doc.data().clockInMinute;
+			clockOutData.innerHTML = doc.data().clockOutHour + ":" + doc.data().clockOutMinute;
+			dateData.innerHTML = doc.id;
+			typeData.innerHTML = doc.data().hourType;
+
+			log.appendChild(dateData);
+			log.appendChild(clockInData);
+			log.appendChild(clockOutData);
+			log.appendChild(typeData);
+
+			hourTable.appendChild(log);		
+		});
+		
+		row.appendChild(hourTable);
+
+	
+	})
+	.catch(function(error) {
+		console.log("Error getting documents: ", error);
+	});
+	
+}
+*/
+
+function setup(){
+	searchButton.addEventListener("click", search);
+	newButton.addEventListener("click",newStudent);
+	editButton.addEventListener("click",editStudent);
+}
+
+setup();
+
+
+
+
+
+
+
+
+
+
+
+// !!Warning!! read this
+/*
+!!Warning!!
+you are about to use javascript you may end up throwing your device out the window
+*/
+/*
+const originalTableHTML = dataTableBody.innerHTML;
+var dataTableHTML = '';
+
+//creation of a new student
+function newStudent() {
+	
+	console.log('newStudent');
+	resetTable();
+	
+	var selectedID = IDBox.value;
+	var selectedFirstName = firstNameBox.value;
+	var selectedLastName = lastNameBox.value;
+	var selectedTeam = teamSelect.value;
+	var selectedRole = roleSelect.value;
+	var selectedSubteam = subteamSelect.value;
+	
+	var people = db.collection("Users");
+	
+	if ((selectedID.length == 8) &&
+		(selectedFirstName.length > 0) &&
+		(selectedLastName.length > 0) &&
+		(selectedTeam != "none") &&
+		(selectedRole != "none")
+	   ){
+				
+		var docRef = db.collection("Users").doc(selectedID);
+		docRef.get().then(function(doc){
+
+			if(doc.exists){
+				alert('Cannot create new student, Student exists');
+			}else{
+				docRef.set({
+					firstName: selectedFirstName,
+					lastName: selectedLastName,
+					teamNumber: selectedTeam,
+					subteam: selectedSubteam,
+					role: selectedRole,
+					shopHours: 0,
+					serviceHours: 0
+				});
+				// TODO:
+				// add individual hour logs here
+			}
+				
+		});
+
+
+	} else {
+		alert('new student parameters invalid');
+	}
+	
+}
+
+function editStudent() {
+	
+	console.log('edit');
+	resetTable();
+
+	var selectedID = IDBox.value;
+	var selectedFirstName = firstNameBox.value
+	var selectedLastName = lastNameBox.value;
+	var selectedTeamNumber = teamSelect.value;
+	var selectedRole = roleSelect.value;
+	var selectedSubteam = subteamSelect.value;
+	
+	if (selectedID.length == 8){
+		
+		var docRef = db.collection("Users").doc(selectedID);
+		docRef.get().then(function(doc){
+
+			if(doc.exists){
+				
+				var newFirstName;
+				var newLastName;
+				var newTeamNumber;
+				var newRole;
+				var newSubteam;
+
+				if(selectedFirstName.length >0){
+					newFirstName = selectedFirstName;				
+				} else {
+					newFirstName = doc.data().firstName;
+				}
+
+				if(selectedLastName.length > 0){
+					newLastName = selectedLastName;
+				} else {
+					newLastName = doc.data().lastName;
+				}
+
+				if(selectedTeamNumber != "none"){
+					newTeamNumber = selectedTeamNumber;					
+				} else {
+					newTeamNumber = doc.data().teamNumber;
+				}
+				
+				if(selectedRole != "none"){
+					newRole = selectedRole;
+				} else {
+					newRole = doc.data().role;
+				}
+				
+				//console.log(selectedSubteam);
+				if(selectedSubteam != "none"){
+					newSubteam = selectedSubteam;
+				} else {
+					newSubteam = doc.data().subteam;
+				}
+				
+				docRef.set({
+					firstName: newFirstName,
+					lastName: newLastName,
+					teamNumber: newTeamNumber,
+					role: newRole,
+					subteam: newSubteam,
+					shopHours: doc.data().shopHours,
+					serviceHours: doc.data().serviceHours
+				});
+
+			}else{
+				alert('Student with ID'+selectedID+'not found');
+			}
+				
+		});
+
+
+	} else {
+		alert('fields invalid');
+
+	}
+	
+	// create a new collection/doc for hours that are logged
+}
+
+function renderRowHTML(doc) {
+
+	var row = document.createElement('tr');
+	row.id = doc.id;
+	//row.onclick = expandRow(doc.id);
+
+	var tableID = document.createElement('td');
+	tableID.innerHTML = doc.id;
+	row.appendChild(tableID);
+
+	var tableLastName = document.createElement('td');  
+	tableLastName.innerHTML = doc.data().lastName;
+	row.appendChild(tableLastName);
+
+	var tableFirstName = document.createElement('td'); 
+	tableFirstName.innerHTML = doc.data().firstName;
+	row.appendChild(tableFirstName);
+
+	var tableTeamNumber = document.createElement('td');
+	tableTeamNumber.innerHTML = doc.data().teamNumber;
+	row.appendChild(tableTeamNumber);
+
+	var tableRole = document.createElement('td');
+	tableRole.innerHTML = doc.data().role;
+	row.appendChild(tableRole);
+
+	var tableSubteam = document.createElement('td');
+	tableSubteam.innerHTML = doc.data().subteam;
+	row.appendChild(tableSubteam);
+
+	var tableButton = document.createElement('button');
+	var tableButtonData = document.createElement('td');
+	/*
+	tableButton.onclick = () => {
+		expandRow(row);
+	};
+	
+	tableButton.id = doc.id + "Button";
+	tableButton.className = "expandButton"; 
+	// for the arrow css later
+	tableButtonData.appendChild(tableButton);
+	row.appendChild(tableButtonData);
+
+	dataTableBody.appendChild(row);
+
+}
+
+function resetTable(){
+	//dataTableBody.innerHTML = originalTableHTML;
+	while(dataTableBody.firstChild){
+		dataTableBody.removeChild(dataTableBody.firstChild);
+	}
+	
+}
+/*
+function removeAllChildren(thing){
+	while(thing.firstChild){
+		thing.removeChild(thing.firstChild);
+	}
+}
+
 function search(){
 	
 	console.log('search');
@@ -362,11 +720,3 @@ function expandRow(row){
 	
 }
 */
-
-function setup(){
-	searchButton.addEventListener("click", search);
-	newButton.addEventListener("click",newStudent);
-	editButton.addEventListener("click",editStudent);
-}
-
-setup();
